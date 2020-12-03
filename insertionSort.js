afbf02d0f756644f8e19c22617b0c6e381c56697
Ricky Tsao <Ricky_Tsao@emap.com>
2020-12-03 14:01:37 +0800

    added comment

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,4 @@
+/*
 "user strict";
 
 //When N is guaranteed to be small,
@@ -41,3 +42,35 @@ function insertionSort(unsortedArray) {
 
 let sorted = insertionSort(dataArray);
 console.log(sorted);
+*/
+
+
+// testing 1 2 3
+function subsetSum(numbers, target, partial) {
+  var s, n, remaining;
+
+  partial = partial || [];
+
+  // sum partial
+  s = partial.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+
+  // check if the partial sum is equals to target
+  if (s === target) {
+    console.log("%s=%s", partial.join("+"), target)
+  }
+
+  if (s >= target) {
+    return;  // if we reach the number why bother to continue
+  }
+
+  for (var i = 0; i < numbers.length; i++) {
+    n = numbers[i];
+    remaining = numbers.slice(i + 1);
+    subsetSum(remaining, target, partial.concat([n]));
+  }
+}
+
+subsetSum([3, 3, 3, 3, 4, 4, 4, 6, 6, 8, 12],12);
+
